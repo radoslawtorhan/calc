d68337189e25695832fca571402c7145072d1b10
Radoslaw Torhan <radoslaw.torhan@gmail.com>
2023-01-16 21:39:34 +0100

    Prevention from bad input added nums can only be digits and cannot divide by zero

--- calc.py
+++ calc.py
@@ -14,6 +14,7 @@ class Calculator(QWidget):
         self.add_button = QPushButton("Add")
         self.sub_button = QPushButton("Sub")
         self.multiply_button = QPushButton("Multiply")
+        self.division_button = QPushButton("Divide")
         self.result_label = QLabel("Result:")
         self.result = QLabel("0")
 
@@ -26,6 +27,7 @@ class Calculator(QWidget):
         layout.addWidget(self.add_button)
         layout.addWidget(self.sub_button)
         layout.addWidget(self.multiply_button)
+        layout.addWidget(self.division_button)
         layout.addWidget(self.result_label)
         layout.addWidget(self.result)
 
@@ -36,24 +38,56 @@ class Calculator(QWidget):
         self.add_button.clicked.connect(self.add_numbers)
         self.sub_button.clicked.connect(self.sub_numbers)
         self.multiply_button.clicked.connect(self.multiply_numbers)
-
+        self.division_button.clicked.connect(self.divide_numbers)
     def add_numbers(self):
-        num1 = float(self.line_edit1.text())
-        num2 = float(self.line_edit2.text())
-        result = num1 + num2
-        self.result.setText(str(result))
+        num1 = self.line_edit1.text()
+        num2 = self.line_edit2.text()
+        if num1.isdigit() and num2.isdigit():
+            num1 = float(num1)
+            num2 = float(num2)
+            result = num1 + num2
+            self.result.setText(str(result))
+        else:
+            self.result.setText("ONLY DIGITS ALLOWED")
 
     def sub_numbers(self):
-        num1 = float(self.line_edit1.text())
-        num2 = float(self.line_edit2.text())
-        result = num1 - num2
-        self.result.setText(str(result))
+        num1 = self.line_edit1.text()
+        num2 = self.line_edit2.text()
+        if num1.isdigit() and num2.isdigit():
+            num1 = float(num1)
+            num2 = float(num2)
+            result = num1 - num2
+            self.result.setText(str(result))
+        else:
+            self.result.setText("ONLY DIGITS ALLOWED")
 
     def multiply_numbers(self):
-        num1 = float(self.line_edit1.text())
-        num2 = float(self.line_edit2.text())
-        result = num1 * num2
-        self.result.setText(str(result))
+        num1 = self.line_edit1.text()
+        num2 = self.line_edit2.text()
+        if num1.isdigit() and num2.isdigit():
+            num1 = float(num1)
+            num2 = float(num2)
+            result = num1 * num2
+            self.result.setText(str(result))
+        else:
+            self.result.setText("ONLY DIGITS ALLOWED")
+
+    def divide_numbers(self):
+        num1 = self.line_edit1.text()
+        num2 = self.line_edit2.text()
+        if num1.isdigit() and num2.isdigit():
+            num1 = float(num1)
+            num2 = float(num2)
+            if num2 != 0:
+                result = num1 / num2
+                self.result.setText(str(result))
+            else:
+                self.result.setText("ERROR, DIVIDING BY ZERO?")
+        else:
+            self.result.setText("ONLY DIGITS ALLOWED")
+
+
+
 
 if __name__ == '__main__':
     app = QApplication([])
